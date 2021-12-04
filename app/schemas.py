a67889b8d2d2a4e1d7c1498361606dbda81fd443
Aleksey Timin <atimin@gmail.com>
2021-12-04 01:52:21 +0100

    use 3 leters for language

--- app/schemas.py
+++ app/schemas.py
@@ -4,30 +4,30 @@ import logging
 from pydantic import BaseModel, Field
 
 SUPPORTED_LANGUAGES = [
-    'bg',
-    'cs',
-    'da',
-    'de',
-    'el',
-    'en',
-    'es',
-    'et',
-    'fi',
-    'fr',
-    'hu',
-    'it',
-    'js',
-    'lt',
-    'lv',
-    'nl',
-    'pl',
-    'pt',
-    'ro',
-    'ru',
-    'sk',
-    'sl',
-    'sv',
-    'zh'
+    'bul',
+    'ces',
+    'dan',
+    'deu',
+    'ell',
+    'eng',
+    'spa',
+    'est',
+    'fin',
+    'fra',
+    'hun',
+    'ita',
+    'jpn',
+    'lit',
+    'lav',
+    'nld',
+    'pol',
+    'por',
+    'ron',
+    'rus',
+    'slk',
+    'slv',
+    'swe',
+    'zho',
 ]
 
 logger = logging.getLogger(__name__)
@@ -36,7 +36,7 @@ logger = logging.getLogger(__name__)
 class CreateOrUpdateRequest(BaseModel):
     """Login request"""
     username: str = Field(description="Unique username", min_length=3, max_length=127)
-    lang: str = Field(description="User's language", min_length=2, max_length=2)
+    lang: str = Field(description="User's language", min_length=3, max_length=3)
 
     def __init__(self, **data):
         super().__init__(**data)
